4584f93f207eb1fa46f4c31dbf8bde1735e62216
Lucas Twisk <lucas.twisk@teampicnic.com>
2019-11-26 10:54:33 +0100

    Add fragment generation

---
+++ ern-container-gen-android/src/hull/lib/src/main/java/com/walmartlabs/ern/container/ElectrodeMiniAppFragment.java
@@ -0,0 +1,63 @@
+package com.picnic.android.widget;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.facebook.react.ReactRootView;
+import com.walmartlabs.ern.container.ElectrodeReactActivityDelegate;
+
+public class ElectrodeMiniAppFragment extends Fragment { //implements ElectrodeReactActivityDelegate.BackKeyHandler, PermissionAwareActivity {
+
+    private static final String INITIAL_PROPS = "props";
+    private ElectrodeReactActivityDelegate mReactActivityDelegate;
+    private ReactRootView view;
+
+    public void addInitialProps(@NonNull Bundle bundle) {
+        Bundle args = new Bundle();
+        args.putBundle(INITIAL_PROPS, bundle);
+        setArguments(args);
+    }
+
+    public static ElectrodeMiniAppFragment newInstance(Bundle initialProps) {
+        ElectrodeMiniAppFragment fragment = new ElectrodeMiniAppFragment();
+
+        return fragment;
+    }
+
+    protected String getMiniAppName() {
+        return null;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle initialProps = getArguments().getBundle(INITIAL_PROPS);
+        view = mReactActivityDelegate.createReactRootView(getMiniAppName(), initialProps);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mReactActivityDelegate.onDestroy();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mReactActivityDelegate = new ElectrodeReactActivityDelegate(getActivity());
+    }
+}
